2a7ff6856827dfcf85bc725daf9f5d75fde1d693
Andreas Eberli <andreas@eberli.me>
2019-06-10 11:55:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:20190504 .
+docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:20190510 .
 docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest .
 
-docker push docker-rolling-updated:20190504
+docker push docker-rolling-updated:20190510
 docker push darkcheyenne/docker-rolling-updated:latest
