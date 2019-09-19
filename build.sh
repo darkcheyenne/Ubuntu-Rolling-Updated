d9ed3052a9d0f382ceccd77df4aae9981e0b6597
Andreas Eberli <andreas@eberli.me>
2019-09-19 11:18:20 +0200

    Hinzufügen von Tag für Github Repository

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #/bin/bash
-docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest .
+docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest .
 
 docker push darkcheyenne/docker-rolling-updated:latest
+docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
