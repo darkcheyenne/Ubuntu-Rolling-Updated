f31f5100ed0e9e322b6cf83db83ff3317f7cfa17
Andreas Eberli <andreas@eberli.me>
2019-11-05 07:52:09 +0100

    Aktuelle Version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu:rolling
 
 ENV DEBIAN_FRONTEND=noninteractive 
-RUN apt-get update && apt-get upgrade -y &&  apt-get install -y tzdata && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get upgrade -y &&  apt-get install -y tzdata
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest .
 
 docker push darkcheyenne/docker-rolling-updated:latest
-docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
+# docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
