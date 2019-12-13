01f490bde8d349148334820cd5b5a7020614928e
Andreas Eberli <andreas@eberli.me>
2019-12-13 09:16:40 +0100

    Rückmeldung von Erfolg für Docker Image Bau Prozess an Syslog Server

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #/bin/bash
-docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest .
+logger Start von Erstellung von Docker Image Ubuntu Rolling Updated
+docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest . | grep 'built\|tagged' | logger
 
+logger Upload von Ubuntu Rolling Updated zu Dockerhub
 docker push darkcheyenne/docker-rolling-updated:latest
-# docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
+logger Upload von Ubuntu Rolling Updated zu Github
+docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
