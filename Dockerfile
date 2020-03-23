b7c10e3d144d59e7584b6a17dae9ee4bd261f10c
Andreas Eberli <andreas@eberli.me>
2020-03-23 11:47:52 +0100

    Hinzufügen von lokalem Squid Proxy für Caching

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM ubuntu:rolling
 
+COPY proxy.conf /etc/apt/apt.conf.d/proxy.conf
 ENV DEBIAN_FRONTEND=noninteractive 
 RUN apt-get update && apt-get upgrade -y &&  apt-get install -y tzdata
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 logger Start von Erstellung von Docker Image Ubuntu Rolling Updated
-docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest . | grep 'built\|tagged' | logger
+# docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest . | grep 'built\|tagged' | logger
+docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest .
 
 logger Upload von Ubuntu Rolling Updated zu Dockerhub
 docker push darkcheyenne/docker-rolling-updated:latest
