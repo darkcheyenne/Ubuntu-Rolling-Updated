54d55dd8d39e15a40d6ca0ba5f370f250d116c8a
Andreas Eberli <andreas@eberli.me>
2020-05-29 07:59:15 +0000

    Anpassen auf Ubuntu 20.04, Setzen von Zeitzone

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,18 @@
-FROM ubuntu:rolling
+FROM ubuntu:focal
 
 COPY proxy.conf /etc/apt/apt.conf.d/proxy.conf
-ENV DEBIAN_FRONTEND=noninteractive 
-RUN apt-get update && apt-get upgrade -y &&  apt-get install -y tzdata
+
+## for apt to be noninteractive
+ENV DEBIAN_FRONTEND noninteractive
+ENV DEBCONF_NONINTERACTIVE_SEEN true
+
+## preesed tzdata, update package index, upgrade packages and install needed software
+RUN echo "tzdata tzdata/Areas select Europe" > /tmp/preseed.txt; \
+    echo "tzdata tzdata/Zones/Europe select Berlin" >> /tmp/preseed.txt; \
+    apt-get -qq update && \
+    apt-get -qq install -y apt-utils && \
+    debconf-set-selections /tmp/preseed.txt && \
+    apt-get -qq install -y tzdata && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /tmp/* && \
+    rm -rf /var/tmp/* 
--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #/bin/bash
-logger Start von Erstellung von Docker Image Ubuntu Rolling Updated
-# docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest . | grep 'built\|tagged' | logger
+echo 🚀 Start von Erstellung von Docker Image Ubuntu Rolling Updated
 docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest .
 
-logger Upload von Ubuntu Rolling Updated zu Dockerhub
+echo 🛰 Upload von Ubuntu Rolling Updated zu Dockerhub
 docker push darkcheyenne/docker-rolling-updated:latest
-logger Upload von Ubuntu Rolling Updated zu Github
+echo 🛰 Upload von Ubuntu Rolling Updated zu Github
 docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
