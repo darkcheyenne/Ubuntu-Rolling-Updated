63404181e5bed1cd8644f3f4d1b4daea9a572e0b
Andreas Eberli <andreas@eberli.me>
2020-10-06 06:35:49 +0000

    Update

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 echo 🚀 Start von Erstellung von Docker Image Ubuntu Rolling Updated
-docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest .
+docker build --pull --no-cache -t darkcheyenne/docker-rolling-updated:latest -t docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/ubuntu-rolling-updated:latest .
 
-echo 🛰 Upload von Ubuntu Rolling Updated zu Dockerhub
-docker push darkcheyenne/docker-rolling-updated:latest
-echo 🛰 Upload von Ubuntu Rolling Updated zu Github
-docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/base:latest
+# echo 🛰 Upload von Ubuntu Rolling Updated zu Dockerhub
+# docker push darkcheyenne/docker-rolling-updated:latest
+# echo 🛰 Upload von Ubuntu Rolling Updated zu Github
+# docker push docker.pkg.github.com/darkcheyenne/ubuntu-rolling-updated/ubuntu-rolling-updated:latest
